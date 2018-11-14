d7fdf91faa436917d5b46aeea2116096089027ef
Michaela Müller <pudding.soup@live.de>
2018-11-14 14:13:16 +0100

    testing in jupyter notebook, transformation working

--- scripts/predict_ppi.py
+++ scripts/predict_ppi.py
@@ -8,8 +8,16 @@ parser.add_argument('-i', '--input_fasta_file')
 parser.add_argument('-o', '--output_file')
 args = parser.parse_args()
 
-random_state = 42
+# read the data
+#  1. vectors
+#  2. PPI input
+# translate protein sequence into feature vectors
+#  1. extract each residue and surrounding of sliding window
+#  2. translate single residue into 7-gram -> 3-gram
+gram_7 = "VWLNGEP"
 
+#  3. get vector for 3-grams
+# get labels of data points
 
 MLPClassifier(hidden_layer_sizes=(25, 50, 100),  
               learning_rate_init=(0.001, 0.01),
