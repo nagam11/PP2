219c4cd485753d8e0008712e691f1e292b91e062
Marla Narazani <marla.narazani@tum.de>
2018-11-15 04:04:48 +0100

    solution ex 2

--- scripts/train.py
+++ scripts/train.py
@@ -9,5 +9,10 @@ parser.add_argument('-i', '--input_fasta_file')
 parser.add_argument('-o', '--output_file')
 args = parser.parse_args()
 
-model = biovec.ProtVec(corpus_fname=args.input_fasta_file, n=3, size=100, window=5, sg=1, min_count=2, workers=2)
+# n = 3 (word length; 3-gram) MAF,SAE etc.
+# size = 100 (size of feature-vector), every 3-gram is represented as a vector of size 100
+# window = 5 (max. distance to neighbouring words)
+# sg = 1 (skip-gram algorithm)
+# min_count = 2 (min. number a k-gram has to be in the corpus)
+model = biovec.ProtVec(corpus_fname=args.input_fasta_file, n=3, size=100, window=5, sg=1, min_count=2, workers=4)
 model.save(args.output_file)
